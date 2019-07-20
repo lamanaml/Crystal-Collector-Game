7b9b60d4d6f9f492e98bb646d4aa342599bf55af
lamana <lamanaml@gmail.com>
2019-07-20 14:10:18 -0400

    updating file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,10 @@
 //There will be four crystals displayed as buttons on the page.
 
+
+randomNumber = (Math.floor(Math.random() * 10); 
+console.log(randomNumber)
+$("#random-Number").html(randomNumber)
+
 // The player will be shown a random number at the start of the game.
 
 //When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
