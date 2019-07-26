45dc6b7dcbcaa42cfa372d66539c8b58426c05b1
lamana <lamanaml@gmail.com>
2019-07-25 21:55:24 -0400

    fixed wins losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
   $(document).ready(function() {
+      
     var buttonOptions = []
-    crystalArray = []
+    var crystalArray = []
     var counter = 0;
     var wins = 0;
     var losses = 0;
     var userTotal = 0;
     var numbertoGuess = 0
     var total = 0
-    var sum = 0
+    //var sum = 0
     
 
 //There will be four crystals displayed as buttons on the page.
@@ -27,49 +28,46 @@ for (var i = 0; i < 4; i++) {
  //When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
 
  
- //var button1 = function(){
+
     $("#btn1").click(function(){
     counter = counter + buttonOptions[0] 
+    //sum = crystalArray[crystalArray.length - 1]
     crystalArray.push(counter)
+    winner()
     console.log(buttonOptions[0]) 
     console.log("new total: ", counter) 
     $("#user-Total").html(counter)     
     })
- //}
-//var button2 = function(){
+
+
     $("#btn2").click(function(){
-        console.log(buttonOptions[1])
+    console.log(buttonOptions[1])
     counter = counter + buttonOptions[1]
     crystalArray.push(counter) 
+    winner()
     console.log("new total: ", counter)  
     $("#user-Total").html(counter) 
     })
-//}
-//var button3 = function(){
+
     $("#btn3").click(function(){
     counter = counter +buttonOptions[2] 
     crystalArray.push(counter)
+    winner()
     console.log(buttonOptions[2])
     console.log("new total: ", counter)  
     $("#user-Total").html(counter)
     })
-//}
-//var button4 = function(){
+
     $("#btn4").click(function(){
     counter = counter +buttonOptions[3] 
     crystalArray.push(counter)
+    winner()
     console.log(buttonOptions[3])
     console.log("new total: ", counter)  
     $("#user-Total").html(counter)
     })
     };
-//}
- 
-// button1();
-// button2();
-// button3();
-// button4();
-//startGame();
+
 
  // Your game will hide this amount until the player clicks a crystal.
 //When they do click one, update the player's score counter.
@@ -92,17 +90,14 @@ console.log(numbertoGuess)
 // sum()
 
 
-function sumArray(crystalArray){
-    sum = sum 
-	for(var j = 0; j < crystalArray.length ; j++){
-		if (crystalArray[j]) {
-			sum += crystalArray[j];
-    	}  
-	}
-    
-	return sum;
-}
-sumArray(crystalArray)
+// function sumArray(crystalArray){
+     
+// 	for(var j = 0; j < crystalArray.length ; j++){
+// 			sum += crystalArray[j];
+// 	}
+//     return sum;
+// }
+// sumArray(crystalArray)
 
 
 // for (var i = 0; i < crystalArray.length; i++) {
@@ -110,23 +105,28 @@ sumArray(crystalArray)
 //     console.log(crystalArray[i])
 // }
    //console.log(total)
-  console.log("sum: " + sum)
-    if (sum === numbertoGuess) {
+
+  
+  // console.log(sum)
+  function winner(){
+      console.log("winner")
+    if (counter === numbertoGuess) {
         console.log(numbertoGuess)
         alert("You Won")
         wins++
         $("#your-Wins").html(wins)
-        startGame();
+       // startGame();
         counter = 0 
-         
-        }
+    } 
+    
 //The player loses if their score goes above the random number.
-    else if (sum >= numbertoGuess) {
+    else if (counter >= numbertoGuess) {
         alert("You Lost")
         losses++
         $("#your-Losses").html(losses)
-        startGame(); 
+      //  startGame(); 
         counter = 0 
+       }
        }
 
 //The game restarts whenever the player wins or loses.
@@ -134,5 +134,5 @@ sumArray(crystalArray)
 //When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 
 //The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-
+startGame()
   });
