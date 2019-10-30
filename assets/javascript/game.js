b52c82a82380067a9235eaff670060f8381a5933
lamana <lamanaml@gmail.com>
2019-10-30 10:30:56 -0400

    updated the style of the crystal collector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,21 +6,24 @@
     var wins = 0;
     var losses = 0;
     var userTotal = 0;
-    var numbertoGuess = 0
-    var total = 0
+    var numbertoGuess = 0;
+    var total = 0;
     
-    
-
 //There will be four crystals displayed as buttons on the page.
 // The player will be shown a random number at the start of the game.
-var startGame = function(){
+function resetAll() {
+  crystalArray;
+  
+}
 
+
+var startGame = function(){
     numbertoGuess = (Math.floor(20 + Math.random() * 100)); 
     $("#crystal-Value").text(numbertoGuess);
     $("#user-Total").html(userTotal);
     
-
 for (var i = 0; i < 4; i++) {
+    
     buttonRan = (1 + Math.floor(Math.random() * 12));  
     buttonOptions.push(buttonRan)
  };
@@ -28,32 +31,37 @@ for (var i = 0; i < 4; i++) {
  //When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
 
     $("#btn1").click(function(){
-    counter += buttonOptions[0] 
-    crystalArray.push(counter)
-    winner()
-    $("#user-Total").html(counter)     
+    counter += buttonOptions[0]; 
+    console.log("btn0: " + buttonOptions[0]);
+    crystalArray.push(counter);
+    winner();
+    $("#user-Total").html(counter) ; 
     })
 
     $("#btn2").click(function(){
-    counter += buttonOptions[1]
-    crystalArray.push(counter) 
-    winner() 
-    $("#user-Total").html(counter) 
-    })
+    counter += buttonOptions[1];
+     console.log("btn1: " +  buttonOptions[1]);
+    crystalArray.push(counter) ;
+    winner() ;
+    $("#user-Total").html(counter); 
+    });
 
     $("#btn3").click(function(){
-    counter += buttonOptions[2] 
-    crystalArray.push(counter)
-    winner()
-    $("#user-Total").html(counter)
-    })
+    counter += buttonOptions[2];
+     console.log("btn2: " + buttonOptions[2]);
+    crystalArray.push(counter);
+    winner();
+    $("#user-Total").html(counter);
+    });
 
     $("#btn4").click(function(){
-    counter += buttonOptions[3] 
-    crystalArray.push(counter)
-    winner()
-    $("#user-Total").html(counter)
-    })
+    counter += buttonOptions[3] ;
+     console.log("btn3: " + buttonOptions[3]);
+    crystalArray.push(counter);
+    winner();
+    $("#user-Total").html(counter);
+  
+    });
     };
 
 
@@ -68,22 +76,24 @@ for (var i = 0; i < 4; i++) {
 
   function winner(){
     if (counter === numbertoGuess) {
-        alert("You Won")
-        wins++
-        $("#your-Wins").html(wins)
-        counter = 0 
+      resetAll();
+        alert("You Won");
+        wins++;
+        $("#your-Wins").html(wins);
+        counter = 0;
+         
         
-
-    } 
+     } 
     
 //The player loses if their score goes above the random number.
     else if (counter >= numbertoGuess) {
-        alert("You Lost")
-        losses++
-        $("#your-Losses").html(losses)
-        counter = 0 
-        
-       }
+       resetAll();
+        alert("You Lost");
+        losses++;
+        $("#your-Losses").html(losses);
+        counter = 0;
+         
+       };
        }
 
 //The game restarts whenever the player wins or loses.
--- readme.txt
+++ readme.txt
@@ -1,37 +1,13 @@
-URL to the game:
- https://lamanaml.github.io/Crystal-Collector-Game/
- 
-### Option One: CrystalsCollector Game (Recommended)
+# Crystal Collector Game
 
-![Crystal Collector](Images/1-CrystalCollector.jpg)
+## Introduction
 
-1. [Watch the demo](https://youtu.be/yNI0l2FMeCk).
+> Crystal-Collector-Game is a Javascript app that generates a random number.  The user selects a combination of crystals that have been assigned an unknown value.  The goal is to select the right combination of crystals that will add up with the random number.  
 
-2. The player will have to guess the answer, just like in Word Guess. This time, though, the player will guess with numbers instead of letters. 
+## Code Samples
 
-3. Here's how the app works:
+> You've gotten their attention in the introduction, now show a few code examples. So they get a visualization and as a bonus, make them copy/paste friendly.
 
-   * There will be four crystals displayed as buttons on the page.
+## Installation
 
-   * The player will be shown a random number at the start of the game.
-
-   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
-     * Your game will hide this amount until the player clicks a crystal.
-     * When they do click one, update the player's score counter.
-
-   * The player wins if their total score matches the random number from the beginning of the game.
-
-   * The player loses if their score goes above the random number.
-
-   * The game restarts whenever the player wins or loses.
-
-     * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-   * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-
-##### Option 1 Game design notes
-
-* The random number shown at the start of the game should be between 19 - 120.
-
-* Each crystal should have a random hidden value between 1 - 12.
+> The installation instructions are low priority in the readme and should come at the bottom. The first part answers all their objections and now that they want to use it, show them how.
